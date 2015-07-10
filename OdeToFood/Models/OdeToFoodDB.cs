eb7ba79db4f71550bf29787c525ee1efc31ee909
CK King <ckking70@gmail.com>
2015-07-10 14:59:52 -0500

    Commit during the Working with Data Part 1 Section

---
+++ OdeToFood/Models/OdeToFoodDB.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Models
+{
+    public class OdeToFoodDB : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<RestrauntReview> Reviews { get; set; }
+    }
+}
